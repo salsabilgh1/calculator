c78715ade6854498040b7a8e045920d448b5377b
salsabil <salsabilghennam1@gmail.com>
2025-08-26 17:13:34 +0100

    created functions for basic operations and three variables

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+//operations :
+function add(a,b){
+    return a + b ;
+}
+function subtruct(a,b){
+    return a - b ;
+}
+
+function multiply(a,b){
+    return a * b ;
+}
+
+function divide(a,b){
+    if(b===0){
+        return "error"
+    }{
+        return a / b ;
+    }
+}
+
+let a , b ,operator;
+
