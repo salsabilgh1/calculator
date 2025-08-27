2d8323a5a1934fa1a14fe0747de640352f228fb4
salsabil <salsabilghennam1@gmail.com>
2025-08-27 14:26:47 +0100

    corected the way the calculator dispaly numbers when clicked

--- javascript.js
+++ javascript.js
@@ -45,13 +45,47 @@ function operate(a , b ,operator){
 }
 
 //display result 
+let number=[]
  const display = document.querySelector(".display")
  const button = document.querySelectorAll(".button") ;
- button.forEach(
-    addEventListener("click",(event)=>{
+ button.forEach(elem=>{
+    elem.addEventListener("click",(event)=>{
         let content = event.target.textContent ;
     display.textContent = content
     console.log(event)
+    number.push(content)
  }
 )
+}
 )
+console.log(number)
+
+//how the calculator work
+// for(let i = 0 ;i<3 ;i++){
+//     if(i===1){
+//     button.addEventListener("click", event=>{
+//     a=Number(event.target.textContent);
+// })
+// }
+// else{
+//     if(i===2){
+//         button.addEventListener("click", event=>{
+//     b=Number(event.target.textContent);
+//     })
+//     }
+//     else{
+//         button.addEventListener("click", event=>{
+//     operator=event.target.textContent;
+//     })
+//     }
+
+// }
+// }
+// button.addEventListener("click" , event =>{
+//     let con = event.target.textContent;
+// })
+// if(con==="="){
+//     let result=operate(a,b,operator);
+// }
+// display.textContent=result;
+// console.log(a)
